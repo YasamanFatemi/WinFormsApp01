75a16e097aaf4fe0deea7230173981401363680d
YasamanFatemi <yfatemi49@gmail.com>
2023-10-17 20:08:53 +0330

    Initial commit

---
+++ WinFormsApp01/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int number = int.Parse(textBox1.Text);
+            MessageBox.Show(Fibonacci(number).ToString());
+        }
+        static int Fibonacci(int number)
+        {
+            if (number <= 1)
+            {
+                return number;
+            }
+            else
+            {
+                return Fibonacci(number - 1) + Fibonacci(number - 2);
+            }
+        }
+    }
+}
